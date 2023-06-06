085f10eed351bead26106cfd131d995bbabb9685
Rene Enriquez <enriquezrene.ap@gmail.com>
2023-06-06 12:59:14 -0500

    Update TheNavigation.vue

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-sm bg-light">
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <div class="container-fluid">
       <ul class="navbar-nav">
         <li class="nav-item">
@@ -13,8 +13,8 @@
         </li>
       </ul>
     </div>
-
   </nav>
+
 </template>
 
 <script>
